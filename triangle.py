d19b928623b17ff8164039e20700b0bf6bc1ce67
porttnov <portmakara9178@gmail.com>
2025-10-22 00:11:16 +0300

    Update triangle.py
    add example

--- triangle.py
+++ triangle.py
@@ -11,3 +11,10 @@ def minimum_total(triangle: List[List[int]]) -> int:
             dp[col] = triangle[row][col] + min(dp[col], dp[col + 1])
     
     return dp[0]
+
+# example
+triangle1 = [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]
+triangle2 = [[-1], [2, 3], [1, -1, -3], [4, 2, 1, 3]]
+
+print(minimum_total(triangle1))  # 11
+print(minimum_total(triangle2))  # 0
